b0fc4caaa1ad449adb1b431b1634724617cee4dc
yamy-cheng <1079625486@qq.com>
2023-12-02 05:45:34 +0000

    add tracker

--- evaluate.sh
+++ evaluate.sh
@@ -2,10 +2,10 @@
 
 for i in {1..15}
 do
-    CUDA_VISIBLE_DEVICES=0 vot evaluate --workspace /disk2/shared_dataset/CYM/VOTS2023_Winner  r50_deaot
+    CUDA_VISIBLE_DEVICES=0 vot evaluate --workspace /disk2/shared_dataset/CYM/VOTS2023_Winner  swinb_deaot
 done
 
-vot pack --workspace /disk2/shared_dataset/CYM/VOTS2023_Winner  r50_deaot
+vot pack --workspace /disk2/shared_dataset/CYM/VOTS2023_Winner  swinb_deaot
 # for i in {1..15}
 # do
 #     CUDA_VISIBLE_DEVICES=0 vot evaluate --workspace /data2/cym/VOTS2023_Winner  swinb_dm_deaot
